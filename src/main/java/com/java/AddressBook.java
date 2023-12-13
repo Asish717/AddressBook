28fcd9b1be9b1a123c7cb4343d514b35a57e1e98
Asish <asishkumar717@gmail.com>
2023-12-13 12:39:44 +0530

    Added: Method to edit contact in adddress book

--- src/main/java/com/java/AddressBook.java
+++ src/main/java/com/java/AddressBook.java
@@ -8,9 +8,57 @@ public class AddressBook {
     private String address;
     private String city;
     private String state;
-    private int zip;
-    private int phone;
+    private String zip;
+    private String phone;
     private String email;
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public String getZip() {
+        return zip;
+    }
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
     @Override
     public String toString() {
         return "AddressBook: " +
@@ -23,7 +71,8 @@ public class AddressBook {
                 ", phone=" + phone +
                 ", email='" + email + '\'';
     }
-    public AddressBook(String firstName, String lastName, String address, String city, String state, int zip, int phone, String email){
+    public AddressBook(String firstName, String lastName, String address, String city,
+                       String state, String zip, String phone, String email){
         this.firstName=firstName;
         this.lastName=lastName;
         this.address=address;
